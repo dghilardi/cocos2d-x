65ceca7b9c024feb603a6997649e0a42d9a4623a
Davide Ghilardi <ghilardi.davide@gmail.com>
2016-11-01 16:15:24 +0100

    add generated .d file

---
+++ cocos/platform/android/java/.d
@@ -0,0 +1,4 @@
+/home/ghila/Programmazione/cpp/cocos/takeplace/TakePlace/cocos2d/cocos/platform/android/java/gen/com/enhance/gameservice/IGameTuningService.java : \
+  /home/ghila/Programmazione/cpp/cocos/takeplace/TakePlace/cocos2d/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl
+
+/home/ghila/Programmazione/cpp/cocos/takeplace/TakePlace/cocos2d/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl :
